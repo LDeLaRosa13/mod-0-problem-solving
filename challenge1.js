ebd92ab9842c1bd865b7ebb98292d79de225f40d
Lauren DeLaRosa <lstoneman13@gmail.com>
2023-03-03 16:59:52 -0500

    show challenge questions

---
+++ challenge1.js
@@ -0,0 +1,11 @@
+// 1. Given an array of strings, return only the strings that have exactly 4 characters.
+
+var names = ["daniel", "mike", "polly", "alex"] 
+for(var i = 0; i < names.length; i++) {
+    if(names[i].length === 4)
+    console.log(names[i])
+}
+
+    
+    
+
---
+++ challenge2.js
@@ -0,0 +1,10 @@
+// 1. Start with an array of strings with a mix of uppercase and lowercase letters. Print every word in all lowercase letters.
+// begin with words that have both uppercase and lowercase
+// all words need printed but changed to all lowercase
+// Working with an array that consists of multiple strings
+// may need to establish a variable, define elements
+
+var names = ["James", "lauren", "Chase", "AlLiSoN"]
+for( var i = 0; i < 5; i++) {
+    console.log(names[i].toLowerCase());
+}
